c85e8305cdb1dab9a10f196a367b9b7c27a8b28d
Vasily <vasilin97@gmail.com>
2024-05-30 13:58:52 -0700

    Make two small examples.

--- Demo/Basic.lean
+++ Demo/Basic.lean
@@ -1,2 +1,14 @@
-def hello := "world"
-#check 1
+import Mathlib.Data.Set.Lattice
+
+example : 1+1=2 := by
+  rfl
+
+example (A : Set ℕ) : A ∩ B ⊆ A := by
+  -- intro x hypothesis
+  -- #check hypothesis.left
+  -- exact hypothesis.left
+  sorry
+
+-- Your turn!
+example : ∀ x y z n : ℕ, n > 2 ∧ x * y * z ≠ 0 → x ^ n + y ^ n ≠ z ^ n :=
+  sorry
